e9ee65be338183b404d89a45d31617bfa548de93
Mark Beamer Jr <markbeamerjr@gmail.com>
2018-05-26 01:26:17 -0400

    -Added content type to gendb to support elastic search 6.+ comms.

--- gendb.sh
+++ gendb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ "$(curl -IHEAD -w '%{http_code}' 'localhost:9200/claims' -o /dev/null --connect-timeout 3 --max-time 5)" == "200" ] ; 
+if [ "$(curl -IHEAD -w '%{http_code}' 'localhost:9200/claims' -o /dev/null --connect-timeout 3 --max-time 5)" == "200" ] ;
 then
    echo "Index already exists." ;
    exit 1;
 else
    echo "Index did not exist, creating..." ;
-   curl -X PUT http://localhost:9200/claims -d '{ "settings" : { "number_of_shards" : 1 }, "mappings" : { "claim" : { "properties" : { "value" : { "type" : "nested" }, "suggest_name": { "type": "completion" }, "suggest_desc": { "type": "completion" } } } } }';
+   curl -X PUT http://localhost:9200/claims -H '"Content-Type: application/json"'-d '{ "settings" : { "number_of_shards" : 1 }, "mappings" : { "claim" : { "properties" : { "value" : { "type" : "nested" }, "suggest_name": { "type": "completion" }, "suggest_desc": { "type": "completion" } } } } }';
    exit 0;
 fi
